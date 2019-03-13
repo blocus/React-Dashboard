b2b1ee7f20dd02ac175915a68052fb97b3c8b94a
Ahmed Meftah <ahmed.meftah1993@gmail.com>
2019-03-13 23:14:42 +0100

    create login Component

---
+++ src/views/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Logpan from "/Component/Logpan/Logpan.jsx"
+
+
+export default class Login extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
