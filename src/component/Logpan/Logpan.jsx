b3f1d8fbd21975c22f4134d42621319654a6e3fb
Ahmed Meftah <ahmed.meftah1993@gmail.com>
2019-03-14 00:34:20 +0100

    create login page

--- src/component/Logpan/Logpan.jsx
+++ src/component/Logpan/Logpan.jsx
@@ -1,10 +1,41 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import findByType from './findByType';
+import css from './Logpan.css';
 
-export default class Logpan extends Component {
-  render() {
-    return (
-      <div>
-      </div>
-    );
-  }
+const Title = () => null;
+class Logpan extends Component {
+
+
+   renderTitle() {
+      const {children} = this.props;
+      const title = findByType(children, Title);
+      if (!title) {
+         return null;
+      }
+      return <div className={css.title}>{title.props.children}</div>;
+   }
+
+
+   render() {
+      let childrens = []
+      if (typeof this.props.children !== 'undefined') {
+         this.props.children.map((e, k) => {
+            childrens.push(e)
+            return null
+         })
+      }
+
+      return (<div className="logpan">
+         {this.renderTitle()}
+         {
+            childrens.map((e, key) => {
+               return e
+            })
+         }
+      </div>);
+   }
 }
+
+Logpan.Title = Title;
+
+export default Logpan;
--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
-import Logpan from "/Component/Logpan/Logpan.jsx"
+import Logpan from "component/Logpan/Logpan.jsx"
 
 
-export default class Login extends Component {
+class Login extends Component {
   render() {
     return (
       <div>
+         <Logpan>
+            <Logpan.Title>
+               My Logpan Title
+            </Logpan.Title>
+            <input />
+            <input />
+            <input />
+            <input />
+            <button>fsdfsdfsdf</button>
+         </Logpan>
       </div>
     );
   }
 }
+export default Login;
