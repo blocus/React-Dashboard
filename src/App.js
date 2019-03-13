05a6549a90ecde6aa3a9d62442172d428865ed7b
Ahmed Meftah <ahmed.meftah1993@gmail.com>
2019-03-13 23:37:02 +0100

    update route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React, { Component } from 'react';
+import React, {Component} from "react"
+import Login from "views/Login/Login.jsx"
+import {BrowserRouter as Router} from 'react-router-dom';
+import Route from 'react-router-dom/Route';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-      </div>
-    );
-  }
-}
 
+   render() {
+      return (
+         <Router>
+            <Route path="/" exact strict component={Login}/>
+         </Router>
+      );
+   }
+}
 export default App;
